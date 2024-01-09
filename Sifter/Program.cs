16e147b81bc64b9e71b38999511bd715fab77c87
Farnakeev Alex <Lexity@yandex.ru>
2024-01-09 19:02:23 +0300

    add some code

--- Sifter/Program.cs
+++ Sifter/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void defaultFunct()
+{
+
+}
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+        int ntask = Convert.ToInt32(Console.ReadLine());
+        switch(ntask){
+            case "-l":{
+                Task25();
+                break;
+            };
+            case 27:{
+                Task27();
+                break;
+            };
+            case 29:{
+                Task29();
+                break;
+            };
+            default:{
+                defaultFunct();
+                break;
+            };
+        }
+  }
+}
