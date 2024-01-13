87b12f0e291ca12432057da6ebc89dc266b37c64
Farnakeev Alex <Lexity@yandex.ru>
2024-01-13 22:12:14 +0300

    add start interface

--- Sifter/Program.cs
+++ Sifter/Program.cs
@@ -1,32 +1,42 @@
-﻿void defaultFunct()
+﻿class ResPrg
 {
 
-}
-
-class TestClass
-{
     static void Main(string[] args)
-    {
-        // Display the number of command line arguments.
-        Console.WriteLine(args.Length);
-        int ntask = Convert.ToInt32(Console.ReadLine());
-        switch(ntask){
-            case "-l":{
-                Task25();
-                break;
-            };
-            case 27:{
-                Task27();
-                break;
-            };
-            case 29:{
-                Task29();
-                break;
-            };
-            default:{
-                defaultFunct();
-                break;
-            };
+    {        
+        if (args.Length==0) 
+        {
+                Console.WriteLine(@"Sifter.exe v0.1
+    Возвращает 'слова' не длинее 3 символов.
+    -h - для отображения деталей.");
         }
-  }
+        else{
+            ResPrg cur = new ResPrg();
+            // Console.WriteLine(args.Length);
+            switch(args[0]){
+                case "-line":{
+                    //Task25();
+                    Console.WriteLine(args[1]);
+                    break;
+                };
+                case "-expl":{
+                    cur.Expl();
+                    break;
+                };
+                case "-help":
+                default:{
+                    Console.WriteLine(@"Sifter.exe
+    Доступные флаги:
+        -expl                           - Демонстрирует пример работы на заложенной тестовой строке.
+        -help                           - Показать справку.
+        -line {ряд слов через пробел}   - Передача своего ряда слов.");
+                    break;
+                }
+            }
+        }
+    }
+
+    void Expl( string LineWords = "tray fur day owe height -9 dark 1 chest pledge stake boom" )
+    {
+        Console.WriteLine(LineWords);
+    }
 }
