7ab908a83bd0ef42d2a095cf19a4303a0f25f77d
Farnakeev Alex <Lexity@yandex.ru>
2024-01-20 19:13:44 +0300

    program complete

--- Sifter/Program.cs
+++ Sifter/Program.cs
@@ -1,5 +1,13 @@
 ﻿class ResPrg
 {
+/*  Задача:
+    Программа, которая из имеющегося массива строк
+    формирует новый массив из строк, 
+    длина которых меньше, либо равна 3 символам.
+    Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+    При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
+
+    static private int SignCount = 3 ;
 
     static void Main(string[] args)
     {        
@@ -10,33 +18,80 @@
     -h - для отображения деталей.");
         }
         else{
-            ResPrg cur = new ResPrg();
             // Console.WriteLine(args.Length);
+            string[] NewLineWords;
+
             switch(args[0]){
+
+                case "-l":
                 case "-line":{
-                    //Task25();
-                    Console.WriteLine(args[1]);
+                    
+                    PrintStrArr(args,1);
+                    NewLineWords = TranslateWordsShortLess(args,1);
+                    PrintStrArr(NewLineWords);
                     break;
                 };
+
+                case "-e":
                 case "-expl":{
-                    cur.Expl();
+                    string[] LineWords = {"tray", "fur", "day", "owe", "height", "-9", "dark", "1", "chest", "pledge", "stake", "boom"};
+                    PrintStrArr(LineWords);
+                    NewLineWords = TranslateWordsShortLess(LineWords);
+                    PrintStrArr(NewLineWords);
                     break;
                 };
+
+                case "-h":
                 case "-help":
                 default:{
                     Console.WriteLine(@"Sifter.exe
     Доступные флаги:
-        -expl                           - Демонстрирует пример работы на заложенной тестовой строке.
-        -help                           - Показать справку.
-        -line {ряд слов через пробел}   - Передача своего ряда слов.");
+        -e -expl                            - Демонстрирует пример работы на заложенной тестовой строке.
+        -h -help                            - Показать справку.
+        -l -line {ряд слов через пробел}    - Передача своего ряда слов.");
                     break;
                 }
             }
         }
     }
 
-    void Expl( string LineWords = "tray fur day owe height -9 dark 1 chest pledge stake boom" )
+    static int CountWordsLengthOf( string[] Words , int firstword = 0 ) // ф-я подсчета слов короче SignCount
     {
-        Console.WriteLine(LineWords);
+        int Count = 0;
+        for( int i = firstword; i < Words.Length; i++)
+        {
+            if ( Words[i].Length < SignCount + 1 )
+            {
+                Count++;
+            }
+        }
+        return Count;
+    }
+
+    static string[] TranslateWordsShortLess( string[] Words , int firstword = 0 ) // ф-я перезаписи в новый массив слов короче SignCount
+    {
+        int count = CountWordsLengthOf(Words, firstword);
+        string[] NewLineWords = new string[count];
+
+        count = 0;
+        for (int i = firstword; i < Words.Length; i++)
+        {
+            if(Words[i].Length < SignCount+1)
+            {
+                NewLineWords[count] = Words[i];
+                count++;
+            } 
+        }
+
+        return NewLineWords;
+    }
+
+    static void PrintStrArr( string[] LineWords , int cur = 0)
+    {
+        for( int i = cur; i < LineWords.Length; i++)
+        {
+            Console.Write($"{LineWords[i]} ");    
+        }
+        Console.WriteLine();
     }
 }
